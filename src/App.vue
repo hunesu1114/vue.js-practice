3b15ff163766a20da8a6d2ddb64796ac42d840b4
hunesu Kim <kewcxea@hanmail.net>
2023-03-06 10:13:13 +0900

    0306

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="a in 메뉴들" :key="a">{{ a }} </a>
+  </div>
+
+  <div>
+    <h4 class="red" :style="스타일">{{ products[0] }}</h4>
+    <p>{{ price0 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      price0: 50,
+      price1: 60,
+      price2: 70,
+      스타일: 'color : blue',
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      메뉴들: ['Home', 'Shop', 'About'],
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
@@ -21,6 +41,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
